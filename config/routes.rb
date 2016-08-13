266f3f33a1ab8b9a37b8f906e4db0eb079ae8ebe
Lucia De Mojà <luciademoja@gmail.com>
2016-08-13 12:11:23 +0200

    completing the walkthrough having specs for database tables and relations email and passwords validations communication with the server create data entries register a user sign in a user

--- config/routes.rb
+++ config/routes.rb
@@ -8,6 +8,7 @@ Rails.application.routes.draw do
       mount_devise_token_auth_for 'User', at: 'auth', skip: [:omniauth_callbacks]
 
       post 'data', controller: :performance_data, action: :create, as: :create
+      get 'data', controller: :performance_data, action: :index, as: :index
     end
   end
 end
--- spec/requests/api/v1/performance_data_spec.rb
+++ spec/requests/api/v1/performance_data_spec.rb
@@ -12,6 +12,16 @@ describe 'Performance Data' do
       entry = PerformanceData.last
       expect(entry.data).to eq({'message' => 'Average'})
     end
+  end
 
+  describe 'GET /api/v1/data/' do
+    before do
+      5.times { current_user.performance_data.create(data: {message: 'Average'}) }
+    end
+
+    it 'returns a collection' do
+      get '/api/v1/data/', {}, headers.merge!(credentials)
+      expect(response_json['entries'].count).to eq 5
+    end
   end
 end
